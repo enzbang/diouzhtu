cc77d084e483619ad7f84fbe798481e6042aa964
olivier.ramonat <olivier.ramonat@cfd3ef84-1e2b-0410-997e-811b222993c3>
2007-03-08 23:33:33 +0000

    Adds experimental support of lists
    git-svn-id: https://diouzhtu.googlecode.com/svn/trunk@6 cfd3ef84-1e2b-0410-997e-811b222993c3

--- diouzhtu/src/diouzhtu-block.adb
+++ diouzhtu/src/diouzhtu-block.adb
@@ -35,6 +35,9 @@ package body Diouzhtu.Block is
    function Header (Index : Positive; Block : String) return String;
    --  <h1>, <h2>, ... element
 
+   function List (Index : Positive; Block : String) return String;
+   --  list element <ul>, <ol>
+
    function Paragraph (Index : Positive; Block : String) return String;
    --  <p> element
 
@@ -112,6 +115,85 @@ package body Diouzhtu.Block is
       return To_String (Result);
    end Header;
 
+   function List (Index : Positive; Block : String) return String is
+      Element  : constant array (1 .. 2) of String (1 .. 2) := ("ol", "ul");
+      Tag      : constant array (1 .. 2) of String (1 .. 2) := ("# ", "* ");
+      Get_Tag  : String (1 .. 2);
+      Position : Natural := 0;
+      Result   : Unbounded_String := Null_Unbounded_String;
+   begin
+      if Block'Length < 3 then
+         return Parse (Block_Level, Block, Index);
+      end if;
+
+      Get_Tag := Block (Block'First .. Block'First + 1);
+
+      if Get_Tag = Tag (1) then
+         Position := 1;
+      elsif Get_Tag = Tag (2) then
+         Position := 2;
+      else
+         return Parse (Block_Level, Block, Index);
+      end if;
+
+      declare
+         Last_Level    : Positive := 1;
+         Current_Level : Natural := 0;
+         Start_Line    : Positive := 1;
+         Last          : Positive;
+         Begin_List    : constant String :=
+           '<' & Element (Position) & '>' & ASCII.Lf;
+         End_List      : constant String :=
+           "</" & Element (Position) & '>' & ASCII.Lf;
+         Begin_Item    : constant String := "<li>";
+         End_Item      : constant String := "</li>" & ASCII.Lf;
+      begin
+         Append (Result, Begin_List);
+         for I in Block'First + 2 .. Block'Last loop
+            if Block (I - 1) = ASCII.Lf or else I = Block'Last then
+               --  Count number of # or * at the beginning of line
+               Current_Level := 0;
+               for K in Start_Line .. I - 2 loop
+                  if Block (K) = Get_Tag (1) then
+                     Current_Level := Current_Level + 1;
+                  end if;
+               end loop;
+
+               --  If current_level = 0
+               --  consider this is the same line
+               --
+               --  ex:
+               --  # begin of list with a line too
+               --  long
+               --  # second element
+
+               if Current_Level /= 0 then
+                  if Current_Level > Last_Level then
+                     Append (Result, Begin_List);
+                  elsif Current_Level < Last_Level then
+                     Append (Result, End_List);
+                  end if;
+                  if I = Block'Last then
+                     Last := I;
+                  else
+                     --  Does not add the tag
+                     Last := I - 2;
+                  end if;
+                  Append (Result, Begin_Item & Parse
+                          (Inline_Level,
+                           Block (Start_Line + Current_Level + 1 .. Last)) &
+                           End_Item);
+                  Last_Level := Current_Level;
+                  Start_Line := I;
+               end if;
+            end if;
+         end loop;
+
+         Append (Result, End_List);
+      end;
+      return To_String (Result);
+   end List;
+
    ---------------
    -- Paragraph --
    ---------------
@@ -155,6 +237,7 @@ package body Diouzhtu.Block is
    procedure Register is
    begin
       Register (Block_Level, Header'Access);
+      Register (Block_Level, List'Access);
       Register (Block_Level, Blockquote'Access);
       Register (Block_Level, Paragraph'Access);
    end Register;
