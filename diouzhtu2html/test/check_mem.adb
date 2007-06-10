fe119ac8bfc21ce8864f26090accd24650f84717
olivier.ramonat <olivier.ramonat@cfd3ef84-1e2b-0410-997e-811b222993c3>
2007-06-10 19:55:52 +0000

    Adds check_mem test
    git-svn-id: https://diouzhtu.googlecode.com/svn/trunk@65 cfd3ef84-1e2b-0410-997e-811b222993c3

---
+++ diouzhtu2html/test/check_mem.adb
@@ -0,0 +1,49 @@
+------------------------------------------------------------------------------
+--                               Diouzhtu                                   --
+--                                                                          --
+--                           Copyright (C) 2007                             --
+--                            Olivier Ramonat                               --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.       --
+------------------------------------------------------------------------------
+
+with Diouzhtu.To_HTML;
+with Ada.Text_IO;
+with Ada.Command_Line;
+
+procedure Check_Mem is
+   use Diouzhtu.To_HTML;
+   use Ada.Command_Line;
+   use Ada.Text_IO;
+
+   Wiki : Diouzhtu.Wiki_Information := Diouzhtu.Initialize ("", "", ".");
+
+begin
+
+   declare
+      Filename  : constant String   := Argument (1);
+      Iteration : constant Positive := Positive'Value (Argument (2));
+
+   begin
+
+      for K in 1 .. Iteration loop
+         Put_Line (To_HTML (Wiki, Filename));
+      end loop;
+   end;
+
+exception
+   when others => Put_Line ("Usage : " & Command_Name &
+                              " [FILENAME] [Number of iteration]");
+end Check_Mem;
