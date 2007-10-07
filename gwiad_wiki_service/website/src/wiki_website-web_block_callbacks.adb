229b83edb0b81e77688102411c79470c12b7eda0
olivier.ramonat <olivier.ramonat@cfd3ef84-1e2b-0410-997e-811b222993c3>
2007-10-07 22:26:38 +0000

    Fix menu to work properly when no file in selected directory
    git-svn-id: https://diouzhtu.googlecode.com/svn/trunk@100 cfd3ef84-1e2b-0410-997e-811b222993c3

--- gwiad_wiki_service/website/src/wiki_website-web_block_callbacks.adb
+++ gwiad_wiki_service/website/src/wiki_website-web_block_callbacks.adb
@@ -21,11 +21,11 @@
 
 with GNAT.Calendar.Time_IO;
 
-with Ada.Strings.Fixed;
 with Ada.Strings.Unbounded;
 
 with Wiki_Website.Config;
 with Wiki_Website.Service;
+with Wiki_Website.Directories;
 with Wiki_Website.Template_Defs.Top;
 with Wiki_Website.Template_Defs.Bottom;
 with Wiki_Website.Template_Defs.Block_Menu;
@@ -66,19 +66,12 @@ package body Wiki_Website.Web_Block_Callbacks is
       use AWS.Status;
       use Ada.Directories;
 
-      Get_URI        : constant String := URI (Request);
-      Name           : constant Wiki_Name := Get_Wiki_Name (Request);
-      HTML_Root      : constant String
-        := Current_Directory & Directory_Separator
-          & Wiki_HTML_Dir (Name);
-      HTML_Directory : constant String
-        := Current_Directory & Directory_Separator
-          & Containing_Directory (Wiki_HTML_Dir (Name)
-                                  & Directory_Separator
-                                  & Get_Directory (Get_URI));
-
       procedure Menu
-        (From : in String; Filenames : in out Templates.Vector_Tag);
+        (From          : in String;
+         Root          : in String;
+         Selected_Dir  : in String;
+         Filenames     : in out Templates.Vector_Tag;
+         Simple_Names  : in out Templates.Vector_Tag);
       --  Creates the menu
 
       ----------
@@ -86,12 +79,20 @@ package body Wiki_Website.Web_Block_Callbacks is
       ----------
 
       procedure Menu
-        (From : in String; Filenames : in out Templates.Vector_Tag) is
+        (From          : in String;
+         Root          : in String;
+         Selected_Dir  : in String;
+         Filenames     : in out Templates.Vector_Tag;
+         Simple_Names  : in out Templates.Vector_Tag)
+      is
          S : Search_Type;
          D : Directory_Entry_Type;
          use type Templates.Vector_Tag;
       begin
-         Filenames := Filenames & Template_Defs.Block_Menu.Set.SET_BEGIN_BLOCK;
+         Filenames    := Filenames
+           & Template_Defs.Block_Menu.Set.SET_BEGIN_BLOCK;
+         Simple_Names := Simple_Names & "";
+
          Start_Search (Search    => S,
                        Directory => From,
                        Pattern   => "*",
@@ -103,53 +104,101 @@ package body Wiki_Website.Web_Block_Callbacks is
             Add_To_Menu :
             declare
                Name      : constant String := Simple_Name (D);
-               Full_Name : constant String := Directories.Full_Name (D);
+               Full_Name : constant String := Ada.Directories.Full_Name (D);
             begin
                if Name /= "." and then Name /= ".." then
                   if Kind (D) = Directory  then
-                     Filenames := Filenames
-                       & (Full_Name (Full_Name'First + HTML_Root'Length
-                                   .. Full_Name'Last) & '/');
-                     if Full_Name'Length <= HTML_Directory'Length
-                       and then
-                         Full_Name = HTML_Directory
-                           (HTML_Directory'First ..
-                                  HTML_Directory'First + Full_Name'Length - 1)
-                     then
-                        Menu (Full_Name, Filenames);
-                     else
 
-                        Ada.Text_IO.Put_Line
-                          (Full_Name & " /= " & HTML_Directory
-                           & " ??? " & Current_Directory);
+                     Filenames    := Filenames
+                       & (Full_Name (Full_Name'First + Root'Length
+                       .. Full_Name'Last) & '/');
+
+                     Simple_Names := Simple_Names & String'(Name & '/');
+
+                     if Full_Name'Length <= Selected_Dir'Length
+                       and then
+                         Full_Name = Selected_Dir
+                           (Selected_Dir'First ..
+                                  Selected_Dir'First + Full_Name'Length - 1)
+                     then
+                        --  This directory is contained into
+                        --  Selected_HTML_Directory. We want subdirs to be in
+                        --  menu too.
+
+                        Menu (From         => Full_Name,
+                              Root         => Root,
+                              Selected_Dir => Selected_Dir,
+                              Filenames    => Filenames,
+                              Simple_Names => Simple_Names);
                      end if;
                   else
-                     Filenames := Filenames
-                       & Full_Name (Full_Name'First + HTML_Root'Length
-                                    .. Full_Name'Last);
 
+                     Filenames := Filenames
+                       & Full_Name
+                       (Full_Name'First + Root'Length .. Full_Name'Last);
+
+                     Simple_Names := Simple_Names & Name;
                   end if;
                end if;
             end Add_To_Menu;
          end loop;
-         Filenames := Filenames & Template_Defs.Block_Menu.Set.SET_END_BLOCK;
+
+         Filenames    := Filenames
+           & Template_Defs.Block_Menu.Set.SET_END_BLOCK;
+         Simple_Names := Simple_Names & "";
+
       end Menu;
 
+      Get_URI   : constant String    := URI (Request);
+      Name      : constant Wiki_Name := Get_Wiki_Name (Request);
+      HTML_Root : constant String    :=
+                    Current_Directory & Directory_Separator
+                      & Wiki_HTML_Dir (Name);
 
-      Filenames : Templates.Vector_Tag;
+      Relative_Selected_Dir : constant String :=
+                                Containing_Directory
+                                  (Wiki_HTML_Dir (Name)
+                                   & Directory_Separator
+                                   & Get_Directory (Get_URI));
+
+      Filenames    : Templates.Vector_Tag;
+      Simple_Names : Templates.Vector_Tag;
    begin
 
       Templates.Insert
         (Translations,
          Templates.Assoc (Template_Defs.Top.WIKI_NAME, String (Name)));
 
+      if Get_URI (Get_URI'Last) = '/' then
 
-      Menu (Wiki_HTML_Dir (Name), Filenames);
+         --  Selected directory is the directory containing a file
+
+         Menu (From         => Wiki_HTML_Dir (Name),
+               Root         => HTML_Root,
+               Selected_Dir => Current_Directory & Directory_Separator
+                 & Ada.Directories.Containing_Directory
+                 (Directories.Get_First_Filename (Relative_Selected_Dir)),
+               Filenames    => Filenames,
+               Simple_Names => Simple_Names);
+      else
+         Menu (From         => Wiki_HTML_Dir (Name),
+               Root         => HTML_Root,
+               Selected_Dir =>
+                 Current_Directory & Directory_Separator
+                 & Relative_Selected_Dir,
+               Filenames    => Filenames,
+               Simple_Names => Simple_Names);
+      end if;
 
       Templates.Insert
         (Translations,
          Templates.Assoc (Variable => Template_Defs.Block_Menu.NAME_V,
                           Value    => Filenames));
+
+      Templates.Insert
+        (Translations,
+         Templates.Assoc (Variable => Template_Defs.Block_Menu.SIMPLE_NAME_V,
+                          Value    => Simple_Names));
    end Menu;
 
    -------------------
@@ -189,75 +238,8 @@ package body Wiki_Website.Web_Block_Callbacks is
                            & Filename;
       HTML_Text      : Unbounded_String := Null_Unbounded_String;
 
-      function Get_First_Filename (Dir : in String) return String;
-      --  Get first file in directory (or subdirectories)
-
       procedure View_File (View_Filename : in String);
       --  View the given file
-
-      ------------------------
-      -- Get_First_Filename --
-      ------------------------
-
-      function Get_First_Filename (Dir : in String) return String is
-         S : Search_Type;
-         D : Directory_Entry_Type;
-      begin
-
-         Start_Search (Search    => S,
-                       Directory => Dir,
-                       Pattern   => "*",
-                       Filter    => Filter_Type'(Ordinary_File => True,
-                                                 Directory     => False,
-                                                 Special_File  => False));
-
-         if More_Entries (S) then
-            Get_Next_Entry (S, D);
-            In_Current_Directory :
-            declare
-               Full_Name : constant String := Directories.Full_Name (D);
-            begin
-               return Strings.Fixed.Delete (Source  => Full_Name,
-                                            From    => Full_Name'First,
-                                            Through => Full_Name'First +
-                                              Current_Directory'Length);
-            end In_Current_Directory;
-         end if;
-
-         --  Not file in directory. Search in subdirectories
-
-         Start_Search (Search    => S,
-                       Directory => Dir,
-                       Pattern   => "*",
-                       Filter    => Filter_Type'(Ordinary_File => False,
-                                                 Directory     => True,
-                                                 Special_File  => False));
-         while More_Entries (S) loop
-            Get_Next_Entry (S, D);
-            Search_In_Subdirs :
-            declare
-               SN : constant String := Simple_Name (D);
-            begin
-               if SN (SN'First) /= '.' then
-                  Check_If_Non_Empty :
-                  declare
-                     First_Filename : constant String
-                       := Get_First_Filename (Directories.Full_Name (D));
-                  begin
-                     if First_Filename /= "" then
-                        return First_Filename;
-                     end if;
-                  end Check_If_Non_Empty;
-               end if;
-            end Search_In_Subdirs;
-         end loop;
-
-         --  No files found.
-
-         return "";
-      exception
-            when others => return "";
-      end Get_First_Filename;
 
       ---------------
       -- View_File --
@@ -269,8 +251,6 @@ package body Wiki_Website.Web_Block_Callbacks is
            (View_Filename'First + HTML_Directory'Length + 1
             .. View_Filename'Last);
       begin
-
-         Ada.Text_IO.Put_Line (View_Filename);
 
          if View_Filename = "" then
             return;
@@ -348,20 +328,18 @@ package body Wiki_Website.Web_Block_Callbacks is
             View_HTML_File :
             declare
                First_Filename : constant String :=
-                                  Get_First_Filename (HTML_Filename);
+                                  Wiki_Website.Directories.Get_First_Filename
+                                    (HTML_Filename);
             begin
-               Ada.Text_IO.Put_Line ("His " & First_Filename);
                View_File (First_Filename);
             end View_HTML_File;
          else
             View_In_Containing_Directory :
             declare
                First_Filename : constant String :=
-                                  Get_First_Filename
+                                  Wiki_Website.Directories.Get_First_Filename
                                     (Containing_Directory (HTML_Filename));
             begin
-               Ada.Text_IO.Put_Line ("Her " & First_Filename);
-
                View_File (First_Filename);
             end View_In_Containing_Directory;
          end if;
@@ -372,7 +350,7 @@ package body Wiki_Website.Web_Block_Callbacks is
            (Translations,
             Templates.Assoc (Template_Defs.Bottom.MODIFICATION_DATE,
               GNAT.Calendar.Time_IO.Image
-                (Directories.Modification_Time
+                (Ada.Directories.Modification_Time
                    (Name => HTML_Filename), "%Y-%m-%d %T")));
       end if;
    end View;
